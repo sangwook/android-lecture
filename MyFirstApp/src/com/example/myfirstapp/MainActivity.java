5e3f34c6b310ffe4e0cca83b22c12a5f5e6fb441
sangwook <2sangwook@gmail.com>
2012-11-05 11:49:37 +0900

    갤러리의 사진을 우리가 만든 액티비티로 보내는 intent 를 만들고 .SEND 인텐트를 만들면 된다.

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myfirstapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -15,6 +17,15 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //layout 파일을 읽어서 적혀있는데로 뷰 클래스를 만들어서 윈도우에 붙이는 역할ㅓㅓ
+        
+        this.getIntent();
+        if(getIntent().getAction().equals(Intent.ACTION_SEND)) {
+        	Uri uri = (Uri) getIntent().getExtras().getParcelable(Intent.EXTRA_STREAM);
+        	
+        	ImageView iv = (ImageView) findViewById(R.id.imageView);
+        	iv.setImageURI(uri);
+        }
+        
         Button btn = (Button)this.findViewById(R.id.testButton);
         btn.setOnClickListener(new OnClickListener() {
 
