4daeaec77c910c792cb503f629f8777fe3725283
sangwook <2sangwook@gmail.com>
2012-11-12 10:38:09 +0900

    ABC 셀렉트박스 형태가 나오는 Spinner 생성 완료

--- SecondClassLayout/src/com/example/secondclasslayout/MainActivity.java
+++ SecondClassLayout/src/com/example/secondclasslayout/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.secondclasslayout;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity {
@@ -11,8 +12,17 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+       
+        //SpinnerAdapter 는 구현해야 할게 너무 많으니까 일단 String이 들어가는 어댑터로 만듬
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adapter.add("ABC");
+        adapter.add("ABC");
+        adapter.add("ABC");
+        adapter.add("ABC");
+        adapter.add("ABC");
+        adapter.add("ABC");
         Spinner spin = (Spinner) findViewById(R.id.spinnerTest);
+        spin.setAdapter(adapter);
     }
 
     @Override
