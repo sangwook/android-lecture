dcc9e9637c165fbbee62931f18a63b53bd80a166
sangwook <2sangwook@gmail.com>
2012-11-16 09:36:05 +0900

    이렇게하면 에러남. 메인 스레드에서 통신은 막고 있음. 어싱크 태스크 만들거나 스레드 만들어야 함

--- ThirdClassHttp/src/com/example/thirdclasshttp/MainActivity.java
+++ ThirdClassHttp/src/com/example/thirdclasshttp/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.thirdclasshttp;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +21,24 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void onClick(View view) {
+    	HttpClient client = new DefaultHttpClient();
+    	HttpGet get = new HttpGet("http://stock.daum.net/item/main.daum?code=035720");
+    	HttpResponse res;
+		try {
+			res = client.execute(get);
+	    	InputStream is = res.getEntity().getContent();
+	    	//BufferedInputStream br = new BufferedInputStream(is);
+	    	
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @Override
